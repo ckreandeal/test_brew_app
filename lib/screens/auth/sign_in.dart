24ea0ecadd1b7a3532193f818d03542adc16920d
Ckrendel <danich1993@mail.ru>
2020-06-11 18:45:56 +0100

    home + auth + signIn + futh and db connection

---
+++ lib/screens/auth/sign_in.dart
@@ -0,0 +1,39 @@
+import 'package:brew_app/services/auth_service.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0,
+        title: Text('Sign In to Brew App'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: RaisedButton(
+          child: Text('Sign in Anon'),
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('error sign in anon');
+            } else {
+              print('signed in');
+              print(result);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:brew_app/screens/auth/auth.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Auth();
+  }
+}
